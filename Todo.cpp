3aab9e91de0793997d2b8a6a01abab231730fb20
josefnatanael <josefnatanael@gmail.com>
2019-09-19 23:22:50 +0800

    Implemented connected part successfully

--- Todo.cpp
+++ Todo.cpp
@@ -6,8 +6,10 @@
 #include "Helper.h"
 #include "Todo.h"
 #include <algorithm>
+#include <vector>
 
 using std::fill;
+using std::vector;
 
 /***********************************************************************
  * TODO_1: Check the number of liberties of a given stone. Mark the
@@ -23,14 +25,92 @@ using std::fill;
  *   connected stones or liberties.
  ***********************************************************************/
 
+enum class Moves { NONE, UP, DOWN, LEFT, RIGHT };
+
+struct StonePlusPlus {
+	bool isChecked;
+	Stone stone;
+};
+
+vector<Moves> check_move_valid(int row, int col) {
+	vector<Moves> moves_to_check;
+	// Can go up
+	if (row - 1 >= 0) {	
+		moves_to_check.push_back(Moves::UP);
+	}
+	// Can go right
+	if (col + 1 <= 18) {
+		moves_to_check.push_back(Moves::RIGHT);
+	}
+	// Can go down
+	if (row + 1 <= 18) {
+		moves_to_check.push_back(Moves::DOWN);
+	}
+	// Can go left
+	if (col - 1 >= 0) {
+		moves_to_check.push_back(Moves::LEFT);
+	}
+	return moves_to_check;
+}
+
+void recursive_connected_liberty(StonePlusPlus boardpp[][19], int row, int col, bool connected_part[][19], bool liberties[][19], Stone stone) {
+	// If itself is not of the same stone, then it is done
+	if (boardpp[row][col].stone != stone || boardpp[row][col].isChecked) {
+		return;
+	}
+	else {
+		// it is the stone to find and it is not checked
+		boardpp[row][col].isChecked = true;
+		connected_part[row][col] = true;
+		vector<Moves> move_to_do = check_move_valid(row, col);
+		for (Moves move : move_to_do) {
+			switch (move) {
+			case Moves::UP:
+				recursive_connected_liberty(boardpp, row - 1, col, connected_part, liberties, stone);
+				break;
+			case Moves::DOWN:
+				recursive_connected_liberty(boardpp, row + 1, col, connected_part, liberties, stone);
+				break;
+			case Moves::LEFT:
+				recursive_connected_liberty(boardpp, row, col - 1, connected_part, liberties, stone);
+				break;
+			case Moves::RIGHT:
+				recursive_connected_liberty(boardpp, row, col + 1, connected_part, liberties, stone);
+				break;
+			case Moves::NONE:
+				break;
+			default:
+				std::cout << "Why does it arrive here???" << std::endl;
+			}
+		}
+	}
+}
+
+int helper_function(StonePlusPlus boardpp[][19], int row, int col, bool connected_part[][19], bool liberties[][19]) {
+	int total_liberty = 0;
+	recursive_connected_liberty(boardpp, row, col, connected_part, liberties, boardpp[row][col].stone);
+	return total_liberty;
+}
+
 int check_liberties(const Stone board[][19], int row, int col, bool connected_part[][19], bool liberties[][19]) {
-	if (row < 1 || row > 19 || col < 1 || col > 19) {
+	StonePlusPlus boardpp[19][19];
+	if (row < 0 || row > 18 || col < 0 || col > 18) {
 		return -1;
 	}
 	else if (board[row][col] == 0) {
 		return -2;
 	}
-	return -1;
+	else {
+		for (int i = 0; i < 19; ++i) {
+			for (int j = 0; j < 19; ++j) {
+				boardpp[i][j].isChecked = false;
+				boardpp[i][j].stone = board[i][j];
+			}
+		}
+	}
+	int total_liberty = 0;
+	total_liberty = helper_function(boardpp, row, col, connected_part, liberties);
+	return total_liberty;
 }
 
 /***********************************************************************
